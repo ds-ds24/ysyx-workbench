63bc8f0de92761515dfbd2b45c2287b9a28dd7d6
231220000-Dou shuo <19037865212@163.com>
2025-06-10 10:31:42 +0000

    mux41

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,9 +1,9 @@
-#include <bits/stdc++.h>
 #include "Vtop.h"
-#include "verilated_vcd_c.h"
+#include "verilated.h"
 #include <nvboard.h>
+
 static TOP_NAME dut;
-void nvboard_bind_all_pins(TOP_NAME* top);
+void nvboard_bind_all_pins(TOP_NAME* top) ;
 //static void single_cycle() {
   //dut.clk = 0; dut.eval();
   //dut.clk = 1; dut.eval();}
