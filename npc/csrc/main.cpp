a46c5b18a386e73a4d2a48673dc8334aa9284692
tracer-ysyx <tracer@ysyx.org>
2025-06-17 13:57:17 +0000

    > compile NEMU ysyx_22040000 窦硕 Linux ds24 6.8.0-60-generic #63-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 18:51:58 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  13:57:17 up 13 days, 18:09,  1 user,  load average: 0.02, 0.07, 0.03

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,37 +1,28 @@
-#include "verilated.h"
-#include "verilated_vcd_c.h"
 #include "Vtop.h"
+#include "verilated.h"
+#include <nvboard.h>
 
-VerilatedContext* contextp = NULL;
-VerilatedVcdC* tfp = NULL;
-static Vtop* top;
-
-void sim_init(){
-	contextp = new VerilatedContext;
-	tfp = new VerilatedVcdC;
-	top = new Vtop;
-	contextp->traceEverOn(true);
-	top->trace(tfp,0);
-	tfp->open("wave.vcd");
+static TOP_NAME dut;
+void nvboard_bind_all_pins(TOP_NAME* top) ;
+//static void single_cycle() {
+  //dut.clk = 0; dut.eval();
+  //dut.clk = 1; dut.eval();}
+signed main(){
+        nvboard_bind_all_pins(&dut);
+        nvboard_init();
+        //VerilatedContext* contextp = new VerilatedContext;
+       // Vtop* top=new Vtop(contextp);
+       // VerilatedVcdC* tfp = new VerilatedVcdC;
+       // contextp->traceEverOn(true);
+       // top->trace(tfp,0);
+       // tfp->open("wave.vcd");
+        while(1){
+                nvboard_update();
+		dut.eval();
+                //single_cycle();
+        }
+        //tfp->close();
+        //return 0;
 }
 
-void step_and_dump_wave(){
-	top->eval();
-	contextp->timeInc(1);
-	tfp->dump(contextp->time());
-}
 
-int main() {
-  sim_init();
-  while(contextp->time()<100){
-        int a=rand()&1;
-        int b=rand()&1;
-        top->a=a;
-        top->b=b;
-        step_and_dump_wave();
-        assert(top->f == (a ^ b));
-  }
-  tfp->close();
-
-  return 0;
-}
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,7 +1,11 @@
 module top(
-	input a,
-	input b,
-	output f
-);
-	assign f = a^b;
-endmodule
+	input reg [1:0] X0,X1,X2,X3,
+	input reg [1:0] Y,
+	output reg [1:0] F
+	);
+	mux41 my_mux41(
+		.X0(X0), .X1(X1), .X2(X2), .X3(X3),
+		.Y(Y), .F(F)
+	);
+	endmodule
+
