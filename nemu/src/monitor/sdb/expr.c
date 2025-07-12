f1fd0019b4129ca649da9f0d1b95619391245f6d
tracer-ysyx <tracer@ysyx.org>
2025-07-12 03:18:33 +0000

    > compile NEMU ysyx_22040000 窦硕 Linux ds24 6.8.0-60-generic #63-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 18:51:58 UTC 2025 aarch64 aarch64 aarch64 GNU/Linux  03:18:33 up 21 days,  1:28,  3 users,  load average: 0.00, 0.04, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -220,7 +220,7 @@ word_t expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  else if(!check_parentheses(0, nr_token)){
+  else if(!check_parentheses(0, nr_token-1)){
     *success = false;
     return 1;
   }
